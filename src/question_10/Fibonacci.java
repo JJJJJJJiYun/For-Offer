3799f910c9cea98cfc9bbbbaded142f907517487
JJJJJJJiYun <jy07171009@outlook.com>
2019-07-13 22:39:11 +0800

    FEATURE: Q10

---
+++ src/question_10/Fibonacci.java
@@ -0,0 +1,33 @@
+package question_10;
+
+public class Fibonacci {
+
+    public int fibonacciWithRecursion(int n) {
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibonacciWithRecursion(n - 1) + fibonacciWithRecursion(n - 2);
+    }
+
+    public int fibonacciWithArray(int n) {
+        if (n <= 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        int former = 0, latter = 1, result = 0;
+        for (int i = 1; i < n; i++) {
+            result = former + latter;
+            former = latter;
+            latter = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.fibonacciWithRecursion(5));
+        System.out.println(fibonacci.fibonacciWithArray(5));
+    }
+
+}
