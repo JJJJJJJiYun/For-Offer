329c6997c47fb065846e78a30eb7ac7ce904f2cd
JJJJJJJiYun <jy07171009@outlook.com>
2019-07-16 18:21:05 +0800

    FEATURE: Q13

---
+++ src/question_13/MovingRobot.java
@@ -0,0 +1,41 @@
+package question_13;
+
+public class MovingRobot {
+
+    private static int countReachableCube(int rows, int cols, int limit) {
+        if (limit < 0) return 0;
+        boolean[][] array = new boolean[rows][cols];
+        return helper(array, 0, 0, limit);
+    }
+
+    private static int helper(boolean[][] array, int i, int j, int limit) {
+        array[i][j] = true;
+        int up = 0, down = 0, left = 0, right = 0;
+        // 向上
+        if (i - 1 >= 0 && (digitSum(i - 1) + digitSum(j)) <= limit && !array[i - 1][j])
+            up = helper(array, i - 1, j, limit);
+        // 向下
+        if (i + 1 < array.length && (digitSum(i + 1) + digitSum(j)) <= limit && !array[i + 1][j])
+            down = helper(array, i + 1, j, limit);
+        // 向左
+        if (j - 1 >= 0 && (digitSum(i) + digitSum(j - 1)) <= limit && !array[i][j - 1])
+            left = helper(array, i, j - 1, limit);
+        // 向右
+        if (j + 1 < array[0].length && (digitSum(i) + digitSum(j + 1)) <= limit && !array[i][j + 1])
+            right = helper(array, i, j + 1, limit);
+        return 1 + up + down + left + right;
+    }
+
+    private static int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countReachableCube(20, 20, 15));
+    }
+}
