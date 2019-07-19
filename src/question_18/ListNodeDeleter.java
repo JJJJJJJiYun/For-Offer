015d180dfcd3580f7c30ed459d1ab8ffc2996c4c
JJJJJJJiYun <jy07171009@outlook.com>
2019-07-19 15:25:38 +0800

    FEATURE: Q18

--- src/question_18/ListNodeDeleter.java
+++ src/question_18/ListNodeDeleter.java
@@ -5,8 +5,12 @@ import structure.ListNode;
 public class ListNodeDeleter {
 
     private static ListNode deleteListNode(ListNode head, ListNode target) {
-        if (head == target) {
+        if (head == null)
             return null;
+        if (target == null)
+            return head;
+        if (head == target) {
+            return head.next;
         }
         if (target.next == null) {
             ListNode pre = head;
@@ -23,14 +27,7 @@ public class ListNodeDeleter {
         return head;
     }
 
-    private static void printListNodes(ListNode head) {
-        while (head != null) {
-            System.out.println(head.value);
-            head = head.next;
-        }
-    }
-
-    public static void main(String[] args) {
+    private static void test1() {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
@@ -47,7 +44,64 @@ public class ListNodeDeleter {
         printListNodes(deleteListNode(node1, node5));
         System.out.println("----------删除头节点----------");
         printListNodes(deleteListNode(node6, node6));
+    }
 
+    private static ListNode deleteDuplicateListNode(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode dummy = new ListNode(head.value - 1);
+        dummy.next = head;
+        ListNode node = dummy;
+        while (node != null && node.next != null) {
+            ListNode next = node.next;
+            if (next.value == node.value) {
+                while (next != null && next.value == node.value) {
+                    next = next.next;
+                }
+                // 特殊处理最后一个也被删掉的情况
+                if (next == null) {
+                    ListNode temp = dummy;
+                    while (temp.next != node)
+                        temp = temp.next;
+                    temp.next = null;
+                    node = null;
+                } else {
+                    node.value = next.value;
+                    node.next = next.next;
+                }
+            } else
+                node = node.next;
+        }
+
+        return dummy.next;
+    }
+
+    private static void test2() {
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        printListNodes(deleteDuplicateListNode(node1));
+    }
+
+
+    private static void printListNodes(ListNode head) {
+        while (head != null) {
+            System.out.println(head.value);
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+//        test1();
+        test2();
     }
 
 }
