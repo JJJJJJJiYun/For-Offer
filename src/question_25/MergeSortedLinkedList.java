286b5e2b96f80fd59f0e0a182b562ae795314c0a
JJJJJJJiYun <jy07171009@outlook.com>
2019-07-22 21:38:51 +0800

    FEATURE: Q25

--- src/question_25/MergeSortedLinkedList.java
+++ src/question_25/MergeSortedLinkedList.java
@@ -4,6 +4,9 @@ import structure.ListNode;
 
 public class MergeSortedLinkedList {
 
+    /**
+     * 合并两个有序链表
+     */
     private static ListNode mergeSortedLinkedList(ListNode head1, ListNode head2) {
         if (head1 == null && head2 == null)
             return null;
