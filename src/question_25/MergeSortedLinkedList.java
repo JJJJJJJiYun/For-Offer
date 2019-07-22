78da2ecac6d195a1fba7d473629457fd4f0e29d2
JJJJJJJiYun <jy07171009@outlook.com>
2019-07-22 21:38:34 +0800

    FEATURE: Q25

---
+++ src/question_25/MergeSortedLinkedList.java
@@ -0,0 +1,54 @@
+package question_25;
+
+import structure.ListNode;
+
+public class MergeSortedLinkedList {
+
+    private static ListNode mergeSortedLinkedList(ListNode head1, ListNode head2) {
+        if (head1 == null && head2 == null)
+            return null;
+        if (head1 != null && head2 == null)
+            return head1;
+        if (head1 == null && head2 != null)
+            return head2;
+        ListNode dummy = new ListNode(Math.min(head1.value, head2.value) - 1);
+        ListNode node = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.value < head2.value) {
+                node.next = head1;
+                head1 = head1.next;
+            } else {
+                node.next = head2;
+                head2 = head2.next;
+            }
+            node = node.next;
+        }
+        if (head1 != null)
+            node.next = head1;
+        else
+            node.next = head2;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3_1 = new ListNode(3);
+        ListNode node3_2 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3_1;
+        node3_2.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        ListNode node = mergeSortedLinkedList(node1, node3_2);
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+
+}
